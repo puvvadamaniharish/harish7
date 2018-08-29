c151b8c78c3f6ab98a63e194d18d66202ca31892
puvvadamaniharish <41737685+puvvadamaniharish@users.noreply.github.com>
2018-08-29 10:53:43 +0530

    Create string1.java

---
+++ string1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner (System.in);
+		String st = s.next();
+		int a = s.nextInt();
+		for (int i=0;i<a;i++)
+		{
+		    char ch = st.charAt(i);
+		    System.out.print(ch);
+		}
+	
+	}
+}
